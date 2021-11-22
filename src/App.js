a721299dc148eaf7a63d94219dbe9b69b2e83708
dmitry-sharashkin <faustfbs@mail.ru>
2021-11-22 17:23:23 +0300

    Установка Material UI, пробный вывод его компонентов

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react'
 import './App.css';
+import axios from "axios";
+
+import Button from '@mui/material/Button';
 
 function App() {
+    const [item,setItem] = useState([])
+    useEffect(()=>{
+        console.log('render')
+        getShares()
+    })
+    const getShares = () => {
+        axios.get('https://financialmodelingprep.com/api/v3/profile/AAPL?apikey=4d1255154b11183450dca74fb6df030a')
+            .then(function (response) {
+                // handle success
+                    console.log(response.data)
+            })
+            .catch(e=>{
+                console.log(e)
+            })
+    }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div  className="App">
+        <h1>{item}</h1>
+        <img src={'https://yt3.ggpht.com/ytc/AKedOLS2SF7GWShAo24fNcUgHqyYBc4dCqaqikWch8gQiw=s900-c-k-c0x00ffffff-no-rj'} alt='APPL'/>
+        <span>Appl</span>
+        <span>123$</span>
+        <Button variant="contained">Hello World</Button>
     </div>
   );
 }
+
 
 export default App;
