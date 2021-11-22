0d15f53531f3ff4ea6d2afd0e2a1f528829d0ffc
dmitry-sharashkin <faustfbs@mail.ru>
2021-11-22 22:36:24 +0300

    добавил header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, {useEffect, useState} from 'react'
 import './App.css';
 import axios from "axios";
-
-import Button from '@mui/material/Button';
+import Header from "./components/Header";
+import Button from "@mui/material/Button";
+import {Container} from "@mui/material";
 
 function App() {
-    const [item,setItem] = useState([])
-    useEffect(()=>{
+    const [item, setItem] = useState([])
+    useEffect(() => {
         console.log('render')
         getShares()
     })
@@ -14,22 +15,30 @@ function App() {
         axios.get('https://financialmodelingprep.com/api/v3/profile/AAPL?apikey=4d1255154b11183450dca74fb6df030a')
             .then(function (response) {
                 // handle success
-                    console.log(response.data)
+                console.log(response.data)
             })
-            .catch(e=>{
+            .catch(e => {
                 console.log(e)
             })
     }
 
-  return (
-    <div  className="App">
-        <h1>{item}</h1>
-        <img src={'https://yt3.ggpht.com/ytc/AKedOLS2SF7GWShAo24fNcUgHqyYBc4dCqaqikWch8gQiw=s900-c-k-c0x00ffffff-no-rj'} alt='APPL'/>
-        <span>Appl</span>
-        <span>123$</span>
-        <Button variant="contained">Hello World</Button>
+    return <div>  
+
+        <Header/>
+        <Container>
+            <div className="App">
+                <h1>{item}</h1>
+                <img
+                    src={'https://yt3.ggpht.com/ytc/AKedOLS2SF7GWShAo24fNcUgHqyYBc4dCqaqikWch8gQiw=s900-c-k-c0x00ffffff-no-rj'}
+                    alt='APPL'/>
+                <span>Appl</span>
+                <span>123$</span>
+                <Button variant="contained">Hello World</Button>
+
+            </div>
+        </Container>
     </div>
-  );
+
 }
 
 
---
+++ src/components/Container.js
@@ -0,0 +1,8 @@
+import React, {useEffect, useState} from 'react';
+import Header from "./Header";
+import Button from "@mui/material/Button";
+import axios from "axios";
+
+
+const Container = () =>
+export default Container
