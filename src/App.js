21e2587585461c3ea8d45ccaeccbe4e104666b8f
dmitry-sharashkin <faustfbs@mail.ru>
2021-11-24 18:47:15 +0300

    Добавил лист акций сбоку

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React, {useEffect, useState} from 'react'
 import './App.css';
 import axios from "axios";
 import Header from "./components/Header";
-import Button from "@mui/material/Button";
 import {Container} from "@mui/material";
+import SharesList from "./components/SharesList";
 
 function App() {
     const [item, setItem] = useState([])
@@ -15,27 +15,28 @@ function App() {
         axios.get('https://financialmodelingprep.com/api/v3/profile/AAPL?apikey=4d1255154b11183450dca74fb6df030a')
             .then(function (response) {
                 // handle success
-                console.log(response.data)
+                console.log(response.data[0])
             })
             .catch(e => {
                 console.log(e)
             })
     }
-
-    return <div>  
+    return <div>
 
         <Header/>
         <Container>
             <div className="App">
-                <h1>{item}</h1>
-                <img
-                    src={'https://yt3.ggpht.com/ytc/AKedOLS2SF7GWShAo24fNcUgHqyYBc4dCqaqikWch8gQiw=s900-c-k-c0x00ffffff-no-rj'}
-                    alt='APPL'/>
-                <span>Appl</span>
-                <span>123$</span>
-                <Button variant="contained">Hello World</Button>
-
+                <div>
+                    <h1>{item}</h1>
+                    <img
+                        src={'https://yt3.ggpht.com/ytc/AKedOLS2SF7GWShAo24fNcUgHqyYBc4dCqaqikWch8gQiw=s900-c-k-c0x00ffffff-no-rj'}
+                        alt='APPL'/>
+                    <span>Appl</span>
+                    <span>123$</span>
+                </div>
+                <SharesList/>
             </div>
+
         </Container>
     </div>
 
---
+++ src/components/SharesList.js
@@ -0,0 +1,312 @@
+import React from 'react'
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemText from "@mui/material/ListItemText";
+import ListItemAvatar from "@mui/material/ListItemAvatar";
+import Avatar from "@mui/material/Avatar";
+import Typography from "@mui/material/Typography";
+import Divider from "@mui/material/Divider";
+
+const SharesList = () => {
+    return(
+        <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+            <ListItem>
+                <Typography variant="h4" gutterBottom component="div">
+                    Shares List
+                </Typography>
+
+            </ListItem>
+
+            <Divider/>
+            <ListItem>
+                <ListItemButton>
+                    <ListItemAvatar>
+                        <Avatar alt="Cindy Baker" src=" https://cdn.dribbble.com/users/139313/screenshots/1105855/dribstock.jpg" />
+                    </ListItemAvatar>
+                    <ListItemText
+                        primary="Apple corporation"
+                        secondary={
+                            <React.Fragment>
+                                <Typography
+                                    sx={{ display: 'inline' }}
+                                    component="span"
+                                    variant="body2"
+                                    color="text.primary"
+                                >
+                                </Typography>
+                                {'APPL'}
+                            </React.Fragment>
+                        }
+                    />
+                </ListItemButton>
+            </ListItem>
+            <ListItem>
+                <ListItemButton>
+                    <ListItemAvatar>
+                        <Avatar alt="Cindy Baker" src="/static/images/avatar/1.jpg" />
+                    </ListItemAvatar>
+                    <ListItemText
+                        primary="AMD corporation"
+                        secondary={
+                            <React.Fragment>
+                                <Typography
+                                    sx={{ display: 'inline' }}
+                                    component="span"
+                                    variant="body2"
+                                    color="text.primary"
+                                >
+                                </Typography>
+                                {'AMD'}
+                            </React.Fragment>
+                        }
+                    />
+                </ListItemButton>
+            </ListItem>
+            <ListItem>
+                <ListItemButton>
+                    <ListItemAvatar>
+                        <Avatar alt="Cindy Baker" src="/static/images/avatar/1.jpg" />
+                    </ListItemAvatar>
+                    <ListItemText
+                        primary="SBER corporation"
+                        secondary={
+                            <React.Fragment>
+                                <Typography
+                                    sx={{ display: 'inline' }}
+                                    component="span"
+                                    variant="body2"
+                                    color="text.primary"
+                                >
+                                </Typography>
+                                {'SBERP'}
+                            </React.Fragment>
+                        }
+                    />
+                </ListItemButton>
+            </ListItem>
+            <ListItem>
+                <ListItemButton>
+                    <ListItemAvatar>
+                        <Avatar alt="Cindy Baker" src="/static/images/avatar/1.jpg" />
+                    </ListItemAvatar>
+                    <ListItemText
+                        primary="SBER corporation"
+                        secondary={
+                            <React.Fragment>
+                                <Typography
+                                    sx={{ display: 'inline' }}
+                                    component="span"
+                                    variant="body2"
+                                    color="text.primary"
+                                >
+                                </Typography>
+                                {'SBERP'}
+                            </React.Fragment>
+                        }
+                    />
+                </ListItemButton>
+            </ListItem>
+            <ListItem>
+                <ListItemButton>
+                    <ListItemAvatar>
+                        <Avatar alt="Cindy Baker" src="/static/images/avatar/1.jpg" />
+                    </ListItemAvatar>
+                    <ListItemText
+                        primary="SBER corporation"
+                        secondary={
+                            <React.Fragment>
+                                <Typography
+                                    sx={{ display: 'inline' }}
+                                    component="span"
+                                    variant="body2"
+                                    color="text.primary"
+                                >
+                                </Typography>
+                                {'SBERP'}
+                            </React.Fragment>
+                        }
+                    />
+                </ListItemButton>
+            </ListItem>
+            <ListItem>
+                <ListItemButton>
+                    <ListItemAvatar>
+                        <Avatar alt="Cindy Baker" src="/static/images/avatar/1.jpg" />
+                    </ListItemAvatar>
+                    <ListItemText
+                        primary="SBER corporation"
+                        secondary={
+                            <React.Fragment>
+                                <Typography
+                                    sx={{ display: 'inline' }}
+                                    component="span"
+                                    variant="body2"
+                                    color="text.primary"
+                                >
+                                </Typography>
+                                {'SBERP'}
+                            </React.Fragment>
+                        }
+                    />
+                </ListItemButton>
+            </ListItem>
+            <ListItem>
+                <ListItemButton>
+                    <ListItemAvatar>
+                        <Avatar alt="Cindy Baker" src="/static/images/avatar/1.jpg" />
+                    </ListItemAvatar>
+                    <ListItemText
+                        primary="SBER corporation"
+                        secondary={
+                            <React.Fragment>
+                                <Typography
+                                    sx={{ display: 'inline' }}
+                                    component="span"
+                                    variant="body2"
+                                    color="text.primary"
+                                >
+                                </Typography>
+                                {'SBERP'}
+                            </React.Fragment>
+                        }
+                    />
+                </ListItemButton>
+            </ListItem>
+            <ListItem>
+                <ListItemButton>
+                    <ListItemAvatar>
+                        <Avatar alt="Cindy Baker" src="/static/images/avatar/1.jpg" />
+                    </ListItemAvatar>
+                    <ListItemText
+                        primary="SBER corporation"
+                        secondary={
+                            <React.Fragment>
+                                <Typography
+                                    sx={{ display: 'inline' }}
+                                    component="span"
+                                    variant="body2"
+                                    color="text.primary"
+                                >
+                                </Typography>
+                                {'SBERP'}
+                            </React.Fragment>
+                        }
+                    />
+                </ListItemButton>
+            </ListItem>
+            <ListItem>
+                <ListItemButton>
+                    <ListItemAvatar>
+                        <Avatar alt="Cindy Baker" src="/static/images/avatar/1.jpg" />
+                    </ListItemAvatar>
+                    <ListItemText
+                        primary="SBER corporation"
+                        secondary={
+                            <React.Fragment>
+                                <Typography
+                                    sx={{ display: 'inline' }}
+                                    component="span"
+                                    variant="body2"
+                                    color="text.primary"
+                                >
+                                </Typography>
+                                {'SBERP'}
+                            </React.Fragment>
+                        }
+                    />
+                </ListItemButton>
+            </ListItem>
+            <ListItem>
+                <ListItemButton>
+                    <ListItemAvatar>
+                        <Avatar alt="Cindy Baker" src="/static/images/avatar/1.jpg" />
+                    </ListItemAvatar>
+                    <ListItemText
+                        primary="SBER corporation"
+                        secondary={
+                            <React.Fragment>
+                                <Typography
+                                    sx={{ display: 'inline' }}
+                                    component="span"
+                                    variant="body2"
+                                    color="text.primary"
+                                >
+                                </Typography>
+                                {'SBERP'}
+                            </React.Fragment>
+                        }
+                    />
+                </ListItemButton>
+            </ListItem>
+            <ListItem>
+                <ListItemButton>
+                    <ListItemAvatar>
+                        <Avatar alt="Cindy Baker" src="/static/images/avatar/1.jpg" />
+                    </ListItemAvatar>
+                    <ListItemText
+                        primary="SBER corporation"
+                        secondary={
+                            <React.Fragment>
+                                <Typography
+                                    sx={{ display: 'inline' }}
+                                    component="span"
+                                    variant="body2"
+                                    color="text.primary"
+                                >
+                                </Typography>
+                                {'SBERP'}
+                            </React.Fragment>
+                        }
+                    />
+                </ListItemButton>
+            </ListItem>
+            <ListItem>
+                <ListItemButton>
+                    <ListItemAvatar>
+                        <Avatar alt="Cindy Baker" src="/static/images/avatar/1.jpg" />
+                    </ListItemAvatar>
+                    <ListItemText
+                        primary="SBER corporation"
+                        secondary={
+                            <React.Fragment>
+                                <Typography
+                                    sx={{ display: 'inline' }}
+                                    component="span"
+                                    variant="body2"
+                                    color="text.primary"
+                                >
+                                </Typography>
+                                {'SBERP'}
+                            </React.Fragment>
+                        }
+                    />
+                </ListItemButton>
+            </ListItem>
+            <ListItem>
+                <ListItemButton>
+                    <ListItemAvatar>
+                        <Avatar alt="Cindy Baker" src="/static/images/avatar/1.jpg" />
+                    </ListItemAvatar>
+                    <ListItemText
+                        primary="SBER corporation"
+                        secondary={
+                            <React.Fragment>
+                                <Typography
+                                    sx={{ display: 'inline' }}
+                                    component="span"
+                                    variant="body2"
+                                    color="text.primary"
+                                >
+                                </Typography>
+                                {'SBERP'}
+                            </React.Fragment>
+                        }
+                    />
+                </ListItemButton>
+            </ListItem>
+
+        </List>
+    )
+}
+export default SharesList
